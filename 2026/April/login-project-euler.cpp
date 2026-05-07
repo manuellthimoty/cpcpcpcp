0962df57da14e9c1258bcd75859672199b2a900a
manuellthimoty <manuelsilalahi06@gmail.com>
2026-05-07 21:35:40 +0700

    add

---
+++ 2026/April/login-project-euler.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define vll vector<ll>
+#define vint vector<int>
+#define input(a,l,r) for(int i = l ; i < r ; i++) cin >> a[i];
+#define REP(i,l,r) for(int i = l ; i < r ; i++)
+#define REPLL(i,l,r) for(ll i = l ; i < r ; i++)
+
+void solve(){
+    ll ans = 0;
+    for(ll i = 1; i <= 997000 ; i++){
+        if(i % 2 == 1) ans += i * i;
+    }
+    cout << ans << endl;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t=1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
